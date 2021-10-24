2a47b323d892e5def8ade73a0804dde7c70a7eb0
Javier Medarde <javiermedmata@gmail.com>
2021-10-24 22:07:29 +0200

    thirty date theory

---
+++ Curso21_22/Teoria_Fechas/Fechas3.php
@@ -0,0 +1,82 @@
+<?php  
+            $error_fecha1= !isset($_POST["fecha1"]);
+            $error_fecha2= !isset($_POST["fecha2"]);
+            $errores= $error_fecha1 || $error_fecha2;
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+<title> Ejercicio fecha 3</title>
+<meta charset="UTF-8"/>
+<style>
+
+    .formulario{background-color:cyan;border:2px solid black;}
+    .respuesta{background-color:green;border:2px solid black;margin-top: 2em;}
+    h2{text-align:centre}
+    form, .respuesta p{margin-left: 2em;}
+
+</style>
+</head>
+
+<body>
+    <div class="formulario">
+    <h2>Fechas-Formulario3</h2>
+    <form action="Fechas3.php" method="post">
+        <p>
+       <label for="fecha1">Introduzca una fecha</label>
+        <input type="date" name="fecha1" id="fecha1" value="<?php if(isset($_POST["fecha1"])) echo $_POST["fecha1"];?>"/>
+        <?php
+        if(isset($_POST["btnComparar"]) && $error_fecha1){
+           
+                echo "*Campo Vacio*";
+           
+        }
+        ?>
+
+        <label for="fecha2">Introduzca una fecha</label>
+        <input type="date" name="fecha2" id="fecha2" value="<?php if(isset($_POST["fecha2"])) echo $_POST["fecha2"];?>"/>
+        </p>
+
+        <?php
+        if(isset($_POST["btnComparar"]) && $error_fecha2){
+           
+                echo "*Campo Vacio*";
+           
+        }
+        ?>
+       
+            <p><button type="submit" name="btnComparar">Comparar</button></p>
+
+
+    </form>
+</div>
+<?php
+    if(isset($_POST["btnComparar"]) && !$errores){
+
+        $fecha=explode("-",$_POST["fecha1"]);
+        $fecha2=explode("-",$_POST["fecha2"]);
+     
+        $seg1=mktime(0,0,0,$fecha[1],$fecha[2],$fecha[0]);
+        $seg2=mktime(0,0,0,$fecha2[1],$fecha2[2],$fecha2[0]);
+
+        $dias=($seg1-$seg2)/(3600*24);
+
+        $dias=abs(floor($dias));
+
+?>
+<div class="respuesta">
+<h2>Dias de diferencia</h2>
+<p>La diferencias en dias entre las dos fechas elegidas es de <?php echo $dias?></p>
+
+</div>
+<?php
+    }
+?>
+</body>
+
+</html>
