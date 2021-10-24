1049f49353b1a421ea52be22f8ae7651eec56670
Javier Medarde <javiermedmata@gmail.com>
2021-10-24 22:08:33 +0200

    fist example class

---
+++ Curso21_22/Ejercicio1/Index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+<title> Mi primera pagina </title>
+<meta charset="UTF-8"/>
+</head>
+
+<body>
+    <?php
+        echo " <h1>Mi primera pagina curso 21-22</h1>";
+    ?>
+   
+</body>
+
+</html>
