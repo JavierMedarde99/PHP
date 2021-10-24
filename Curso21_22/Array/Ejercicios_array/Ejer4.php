8c15ed091fd83fede65c1c7ff0fe9b26c74db87f
Javier Medarde <javiermedmata@gmail.com>
2021-10-24 21:53:20 +0200

    fourth array exercise

---
+++ Curso21_22/Array/Ejercicios_array/Ejer4.php
@@ -0,0 +1,25 @@
+
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+<title> Ejercicio 4</title>
+<meta charset="UTF-8"/>
+</head>
+
+<body>
+    <h1>Ejercicio 4</h1>
+    
+    <?php
+        $arr[]="Pedro";
+        $arr[]="Ana";
+        $arr[]=34;
+        $arr[]=1;
+
+        print_r($arr);
+    ?>
+   
+</body>
+
+</html>
