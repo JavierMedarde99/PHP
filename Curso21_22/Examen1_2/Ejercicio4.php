044b746d97e3ae5fea7306ae905793a1b9f80942
Javier Medarde <javiermedmata@gmail.com>
2021-11-02 10:49:33 +0100

    keep values for select

--- Curso21_22/Examen1_2/Ejercicio4.php
+++ Curso21_22/Examen1_2/Ejercicio4.php
@@ -8,14 +8,16 @@
 </head>
 <body>
     <h1>Ejercicio4</h1>
-    <form action="Ejercicio4.php" method="post"></form>
+    <form action="Ejercicio4.php" method="post">
     <p><label for="">Seleccione un profesor</label>
     <?php
      @$file=fopen("horarios.txt","r");
      if(!$file)
       die("no se ha podido abrir");
       ?>
+
     <select name="profesor" id="profesor">
+
     <?php
    
 while($fila=fgets($file)){
@@ -25,18 +27,18 @@ echo "<option selected value='".$valores[0]."'>".$valores[0]."</option>";
 else
 echo "<option value='".$valores[0]."'>".$valores[0]."</option>";
 
-for($i=1;$i<count($valores);$i++){
-    if(isset($horario[$valores[0]][$valores[$i]][$valores[$i+1]]))
-    $horario[$valores[0]][$valores[$i]][$valores[$i+1]].="/". $valores[$i+2];
-    else
-$horario[$valores[0]][$valores[$i]][$valores[$i+1]]= $valores[$i+2];
-$i=$i+2;
-}
 }
 fclose($file);
 ?>
     </select>
     <input type="submit" value="Ver Horario" name="btnVer">
+    
 </p>
+</form>
+<?php
+    if(isset($_POST["btnVer"]))
+    echo "<h3>horario del profesor ".$_POST["profesor"]."</h3>"
+    
+    ?>
 </body>
 </html>
