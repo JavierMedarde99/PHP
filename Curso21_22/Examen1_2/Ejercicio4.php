13dc951c4a625d3b42f1c6cf4bf24ac972bddaaa
Javier Medarde <javiermedmata@gmail.com>
2021-11-02 10:28:48 +0100

    inicial code to ejercicio4.2

---
+++ Curso21_22/Examen1_2/Ejercicio4.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio4 version 2</title>
+</head>
+<body>
+    <h1>Ejercicio4</h1>
+    <form action="Ejercicio4.php" method="post"></form>
+    <p><label for="">Seleccione un profesor</label>
+    <select name="profesor" id="profesor">
+    
+    </select>
+    <input type="submit" value="Ver Horario" name="btnVer">
+</p>
+</body>
+</html>
