525ead284e74e4da3883cc1e82f7c866c78292e0
Javier Medarde <javiermedmata@gmail.com>
2021-11-03 13:16:31 +0100

    complete the query

--- Curso21_22/teor_BD/index.php
+++ Curso21_22/teor_BD/index.php
@@ -15,7 +15,19 @@
 
     mysqli_set_charset($conexion,"utf8");
 
+    $consulta="SELECT * FROM alumnos";
+    $resultado=mysqli_query($conexion,$consulta);
+    if($resultado){
 
+        $n_tuplas=mysqli_num_rows($resultado);
+
+        echo "<p>El numero de alumnos en la BD es: ".$n_tuplas." </p>";
+
+    }else{
+
+        die("Imposible realizar la consulta. Error Numero: ".mysqli_errno($conexion)." : ".mysqli_error($conexion));
+
+    }
 
 
 
