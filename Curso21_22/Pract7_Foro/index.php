9615d72d8467d94e168414a0dc4023cffdb29b68
Javier Medarde <javiermedmata@gmail.com>
2021-11-04 10:16:02 +0100

    put the css

--- Curso21_22/Pract7_Foro/index.php
+++ Curso21_22/Pract7_Foro/index.php
@@ -9,9 +9,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Primer CRUD</title>
+    <style>
+        .centrar{text-align: center;}
+        table,th,tr{border:1px solid black}
+        table{border-collapse: collapse;width: 60%;margin: 0 auto;}
+    </style>
 </head>
 <body>
-    <h1>Listado de usuarios </h1>
+    <h1 class="centrar">Listado de usuarios </h1>
     <table>
         <tr>
             <th>Nombre de Usuario</th>
