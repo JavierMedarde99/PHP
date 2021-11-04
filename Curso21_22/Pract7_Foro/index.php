57987e91cd2b89660532de1ab85a98d89ae45940
Javier Medarde <javiermedmata@gmail.com>
2021-11-04 10:39:50 +0100

    complete the table with the name of that db

--- Curso21_22/Pract7_Foro/index.php
+++ Curso21_22/Pract7_Foro/index.php
@@ -11,20 +11,54 @@
     <title>Primer CRUD</title>
     <style>
         .centrar{text-align: center;}
-        table,th,tr{border:1px solid black}
+        table,th,td{border:1px solid black}
         table{border-collapse: collapse;width: 60%;margin: 0 auto;}
     </style>
 </head>
 <body>
     <h1 class="centrar">Listado de usuarios </h1>
-    <table>
-        <tr>
-            <th>Nombre de Usuario</th>
-            <th>borar</th>
-            <th>editar</th>
-        </tr>
+    
+       
+            
+           
+            
+        
 <?php
+ @$conexion=mysqli_connect(SERVIDOR_BD,USUARIO_BD,CLAVE_BD,NAME_BD);
+ if(!$conexion)
+     die("Imposible conectar. Error Numero: ".mysqli_connect_errno()." : ".mysqli_connect_error());
 
+     mysqli_set_charset($conexion,"utf8");
+
+     $consulta="SELECT * FROM usuarios";
+     $resultado=mysqli_query($conexion,$consulta);
+
+     if($resultado){
+echo "<table>";
+echo " <tr>";
+echo "<th>Nombre de Usuario</th>";
+echo " <th>borar</th>";
+echo "<th>editar</th>";
+echo "</tr>";
+        while($datos=mysqli_fetch_assoc($resultado)){
+             echo "<tr>";
+        echo "<td>".$datos["nombre"]."</td>";
+        echo "<td><img src='imagenes/borrar.png'></td>";
+        echo "<td><img src='imagenes/editar.png'></td>";
+        echo "</tr>";
+        }
+        echo "</table>";
+
+        mysqli_free_result($resultado); 
+        mysqli_close($conexion);
+
+    }else{
+
+        $error ="<p>Imposible realizar la consulta. Error Numero: ".mysqli_errno($conexion)." : ".mysqli_error($conexion)."</p>";
+        mysqli_close($conexion);
+        die($error);
+
+    }   
 ?>
 </table>
 </body>
