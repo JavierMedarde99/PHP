93d2e36ca6cb724fd21bf6fedbb30588110c5a82
Javier Medarde <javiermedmata@gmail.com>
2021-11-04 10:56:05 +0100

    complete form

---
+++ Curso21_22/Pract7_Foro/usuario_nuevo.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nuevo Usuario</title>
+</head>
+<body>
+    <h1>Nuevo Usuario</h1>
+
+    <form action="index.php" method="post">
+
+    <br/>
+    <br/>
+    <label for="nombre">Nombre</label>
+    <input type="text" name="nombre" id="nombre">
+
+    <br/>
+    <br/>
+    <label for="usuario">Usuario</label>
+    <input type="text" name="usuario" id="usuario">
+
+    <br/>
+    <br/>
+    <label for="contraseña">Contraseña</label>
+    <input type="password" name="contraseña" id="contraseña">
+
+    <br/>
+    <br/>
+    <label for="email">Email</label>
+    <input type="text" name="email" id="email">
+
+    </form>
+</body>
+</html>
