e0db181da69c77a7f56c0bd3f38dea37d2711933
Javier Medarde <javiermedmata@gmail.com>
2021-11-04 10:11:10 +0100

    require to config

--- Pract7_Foro/index.php
+++ Pract7_Foro/index.php
@@ -1,12 +1,17 @@
+<?php
+    require "src/config.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Primer CRUD</title>
 </head>
 <body>
-    
+    <h1>Listado de usuarios </h1>
+
 </body>
 </html>
