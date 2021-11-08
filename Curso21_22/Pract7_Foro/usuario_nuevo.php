69402b57739f58b1fcecf9acd3daaf11a0acdc79
Javier Medarde <javiermedmata@gmail.com>
2021-11-08 13:09:11 +0100

    error control

--- Curso21_22/Pract7_Foro/usuario_nuevo.php
+++ Curso21_22/Pract7_Foro/usuario_nuevo.php
@@ -1,6 +1,5 @@
 <?php
-if(isset($_POST["btnVolver"]))
-header('Location: index.php');
+
 
 function repetido($repetirdo){
     @$conexion=mysqli_connect(SERVIDOR_BD,USUARIO_BD,CLAVE_BD,NAME_BD);
@@ -16,8 +15,8 @@ function repetido($repetirdo){
 if(isset($_POST["btnContinuar"])){
     $error_Nombre= $_POST["nombre"]=="";
     $error_Usuario=$_POST["usuario"]=="" || repetido($_POST["usuario"]);
-    $error_Contraseña= $_POST["contraseña"]=="";
-    $error_Email=$_POST["email"]=="";
+    $error_Contraseña= $_POST["contraseña"]=="" ;
+    $error_Email=$_POST["email"]==""|| !filter_var($email,FILTER_VALIDATE_EMAIL);
     $errores=$error_Nombre|| $error_Usuario || $error_Contraseña || $error_Email;
 }
 ?>
@@ -39,26 +38,51 @@ if(isset($_POST["btnContinuar"])){
     <br/>
     <label for="nombre">Nombre</label>
     <input type="text" name="nombre" id="nombre">
+<?php
+if(isset($_POST["btnContinuar"]) && $error_Nombre){
+    echo "<p>*Campo vacio*</p>";
+}
 
+?>
     <br/>
     <br/>
     <label for="usuario">Usuario</label>
     <input type="text" name="usuario" id="usuario">
-
+    <?php
+if(isset($_POST["btnContinuar"])){
+    if($_POST["usuario"]=="")
+    echo "<p>*Campo vacio*</p>";
+    else
+    echo "usuario repetido";
+}
+?>
     <br/>
     <br/>
     <label for="contraseña">Contraseña</label>
     <input type="password" name="contraseña" id="contraseña">
+    <?php
+if(isset($_POST["btnContinuar"]) && $error_Contraseña){
+    echo "<p>*Campo vacio*</p>";
+}
 
+?>
     <br/>
     <br/>
     <label for="email">Email</label>
     <input type="email" name="email" id="email">
+    <?php
+if(isset($_POST["btnContinuar"])){
+    if($_POST["email"]=="")
+    echo "<p>*Campo vacio*</p>";
+    else
+    echo "email incorrecto";
+}
 
+?>
     <br/>
     <br/>
     <input type="submit" value="Continuar" name="btnContinuar">
-    <input type="submit" value="Volver" name="btnVolver">
+    <input type="submit" value="Volver" name="btnVolver" formaction="index.php">
     </form>
 </body>
 </html>
