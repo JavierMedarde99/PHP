d29696dceba5512e8c24760cd286eef212918dd9
Javier Medarde <javiermedmata@gmail.com>
2021-11-10 13:42:48 +0100

    change bd_foro3 to bd_foro

--- Curso21_22/Pract7_Foro/src/config.php
+++ Curso21_22/Pract7_Foro/src/config.php
@@ -2,5 +2,5 @@
     define("SERVIDOR_BD","localhost");
     define("USUARIO_BD","jose");
     define("CLAVE_BD","josefa");
-    define("NOMBRE_BD","bd_foro3");
+    define("NOMBRE_BD","bd_foro");
 ?>
